a8ff8bb2b2e73b56cdc2bee65d63c0d1e25603e7
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-09-22 10:28:42 +0530

    tests

--- raft/harness.go
+++ raft/harness.go
@@ -2,6 +2,7 @@ package raft
 
 import (
 	"log"
+	"sync"
 	"testing"
 	"time"
 )
@@ -11,8 +12,19 @@ func init() {
 }
 
 type Harness struct {
+	mu sync.Mutex
+
 	// cluster is a list of all the raft servers participating in a cluster.
 	cluster []*Server
+
+	// commitChans has a channel per server in cluster with the commit channel for
+	// that server.
+	commitChans []chan CommitEntry
+
+	// commits at index i holds the sequence of commits made by server i so far.
+	// It is populated by goroutines that listen on the corresponding commitChans
+	// channel.
+	commits [][]CommitEntry
 
 	// connected has a bool per server in cluster, specifying whether this server
 	// is currently connected to peers (if false, it's partitioned and no messages
@@ -28,6 +40,8 @@ type Harness struct {
 func NewHarness(t *testing.T, n int) *Harness {
 	ns := make([]*Server, n)
 	connected := make([]bool, n)
+	commitChans := make([]chan CommitEntry, n)
+	commits := make([][]CommitEntry, n)
 	ready := make(chan any)
 
 	// Create all Servers in this cluster, assign ids and peer ids.
@@ -39,7 +53,8 @@ func NewHarness(t *testing.T, n int) *Harness {
 			}
 		}
 
-		ns[i] = NewServer(i, peerIds, ready)
+		commitChans[i] = make(chan CommitEntry)
+		ns[i] = NewServer(i, peerIds, ready, commitChans[i])
 		ns[i].Serve()
 	}
 
@@ -54,12 +69,18 @@ func NewHarness(t *testing.T, n int) *Harness {
 	}
 	close(ready)
 
-	return &Harness{
-		cluster:   ns,
-		connected: connected,
-		n:         n,
-		t:         t,
+	h := &Harness{
+		cluster:     ns,
+		commitChans: commitChans,
+		commits:     commits,
+		connected:   connected,
+		n:           n,
+		t:           t,
 	}
+	for i := 0; i < n; i++ {
+		go h.collectCommits(i)
+	}
+	return h
 }
 
 // Shutdown shuts down all the servers in the harness and waits for them to
@@ -71,6 +92,9 @@ func (h *Harness) Shutdown() {
 	}
 	for i := 0; i < h.n; i++ {
 		h.cluster[i].Shutdown()
+	}
+	for i := 0; i < h.n; i++ {
+		close(h.commitChans[i])
 	}
 }
 
@@ -106,7 +130,7 @@ func (h *Harness) ReconnectPeer(id int) {
 // Returns the leader's id and term. It retries several times if no leader is
 // identified yet.
 func (h *Harness) CheckSingleLeader() (int, int) {
-	for r := 0; r < 5; r++ {
+	for r := 0; r < 8; r++ {
 		leaderId := -1
 		leaderTerm := -1
 		for i := 0; i < h.n; i++ {
@@ -144,6 +168,103 @@ func (h *Harness) CheckNoLeader() {
 	}
 }
 
+// CheckCommitted verifies that all connected servers have cmd committed with
+// the same index. It also verifies that all commands *before* cmd in
+// the commit sequence match. For this to work properly, all commands submitted
+// to Raft should be unique positive ints.
+// Returns the number of servers that have this command committed, and its
+// log index.
+func (h *Harness) CheckCommitted(cmd int) (nc int, index int) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	// Find the length of the commits slice for connected servers.
+	commitsLen := -1
+	for i := 0; i < h.n; i++ {
+		if h.connected[i] {
+			if commitsLen >= 0 {
+				// If this was set already, expect the new length to be the same.
+				if len(h.commits[i]) != commitsLen {
+					h.t.Fatalf("commits[%d] = %d, commitsLen = %d", i, h.commits[i], commitsLen)
+				}
+			} else {
+				commitsLen = len(h.commits[i])
+			}
+		}
+	}
+
+	// Check consistency of commits from the start and to the command we're asked
+	// about. This loop will return once a command=cmd is found.
+	for c := 0; c < commitsLen; c++ {
+		cmdAtC := -1
+		for i := 0; i < h.n; i++ {
+			if h.connected[i] {
+				cmdOfN := h.commits[i][c].Command.(int)
+				if cmdAtC >= 0 {
+					if cmdOfN != cmdAtC {
+						h.t.Errorf("got %d, want %d at h.commits[%d][%d]", cmdOfN, cmdAtC, i, c)
+					}
+				} else {
+					cmdAtC = cmdOfN
+				}
+			}
+		}
+		if cmdAtC == cmd {
+			// Check consistency of Index.
+			index := -1
+			nc := 0
+			for i := 0; i < h.n; i++ {
+				if h.connected[i] {
+					if index >= 0 && h.commits[i][c].Index != index {
+						h.t.Errorf("got Index=%d, want %d at h.commits[%d][%d]", h.commits[i][c].Index, index, i, c)
+					} else {
+						index = h.commits[i][c].Index
+					}
+					nc++
+				}
+			}
+			return nc, index
+		}
+	}
+
+	// If there's no early return, we haven't found the command we were looking
+	// for.
+	h.t.Errorf("cmd=%d not found in commits", cmd)
+	return -1, -1
+}
+
+// CheckCommittedN verifies that cmd was committed by exactly n connected
+// servers.
+func (h *Harness) CheckCommittedN(cmd int, n int) {
+	nc, _ := h.CheckCommitted(cmd)
+	if nc != n {
+		h.t.Errorf("CheckCommittedN got nc=%d, want %d", nc, n)
+	}
+}
+
+// CheckNotCommitted verifies that no command equal to cmd has been committed
+// by any of the active servers yet.
+func (h *Harness) CheckNotCommitted(cmd int) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	for i := 0; i < h.n; i++ {
+		if h.connected[i] {
+			for c := 0; c < len(h.commits[i]); c++ {
+				gotCmd := h.commits[i][c].Command.(int)
+				if gotCmd == cmd {
+					h.t.Errorf("found %d at commits[%d][%d], expected none", cmd, i, c)
+				}
+			}
+		}
+	}
+}
+
+// SubmitToServer submits the command to serverId.
+func (h *Harness) SubmitToServer(serverId int, cmd any) bool {
+	return h.cluster[serverId].cm.Submit(cmd)
+}
+
 func tlog(format string, a ...any) {
 	format = "[TEST] " + format
 	log.Printf(format, a...)
@@ -151,4 +272,16 @@ func tlog(format string, a ...any) {
 
 func sleepMs(n int) {
 	time.Sleep(time.Duration(n) * time.Millisecond)
+}
+
+// collectCommits reads channel commitChans[i] and adds all received entries
+// to the corresponding commits[i]. It's blocking and should be run in a
+// separate goroutine. It returns when commitChans[i] is closed.
+func (h *Harness) collectCommits(i int) {
+	for c := range h.commitChans[i] {
+		h.mu.Lock()
+		tlog("collectCommits(%d) got %+v", i, c)
+		h.commits[i] = append(h.commits[i], c)
+		h.mu.Unlock()
+	}
 }
